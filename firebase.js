319037ba3ea0f905f7bc71bc4b7863ea82b9841f
MGTAssign <daleydemaulie87@gmail.com>
2026-05-02 18:15:06 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxWrtow_qvplxLYRXFuiYa2V4zwawv0Ss",
+  authDomain: "assignment-tracker-8c7b9.firebaseapp.com",
+  projectId: "assignment-tracker-8c7b9",
+  storageBucket: "assignment-tracker-8c7b9.firebasestorage.app",
+  messagingSenderId: "254719298457",
+  appId: "1:254719298457:web:13a175e0b91a7a626d6108"
+};
+
+// COMPAT MODE 
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
