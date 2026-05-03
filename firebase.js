324b450f9d4e5ebad349ddd30cb54b4afad69de9
MGTAssign <daleydemaulie87@gmail.com>
2026-05-02 21:55:42 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,19 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBxWrtow_qvplxLYRXFuiYa2V4zwawv0Ss",
-  authDomain: "assignment-tracker-8c7b9.firebaseapp.com",
-  projectId: "assignment-tracker-8c7b9",
-  storageBucket: "assignment-tracker-8c7b9.firebasestorage.app",
-  messagingSenderId: "254719298457",
-  appId: "1:254719298457:web:13a175e0b91a7a626d6108"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// 🔐 AUTH
 const auth = firebase.auth();
+
+// 📦 FIRESTORE (THIS IS CRITICAL)
 const db = firebase.firestore();
+
+console.log("Firebase initialized");
