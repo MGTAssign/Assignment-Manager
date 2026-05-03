975579764d7d30b672f8b188fcd5f8b903fa9b68
MGTAssign <daleydemaulie87@gmail.com>
2026-05-02 22:46:40 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyBxWrtow_qvplxLYRXFuiYa2V4zwawv0Ss",
   authDomain: "assignment-tracker-8c7b9.firebaseapp.com",
   projectId: "assignment-tracker-8c7b9",
-  storageBucket: "assignment-tracker-8c7b9.appspot.com", // ✅ FIXED
+  storageBucket: "assignment-tracker-8c7b9.appspot.com",
   messagingSenderId: "254719298457",
-  appId: "1:254719298457:web:13a175e0b91a7a626d6108",
-  measurementId: "G-BCKEXP7PVG"
+  appId: "1:254719298457:web:13a175e0b91a7a626d6108"
 };
 
+// INIT FIREBASE (MUST COME AFTER IMPORTS)
 firebase.initializeApp(firebaseConfig);
 
+// SERVICES
 const auth = firebase.auth();
 const db = firebase.firestore();
 
